2654b3d67d3eb89356fdd2b22cb67f5c45668b01
Ayoub-fetti <ayoubfetti3@gmail.com>
2025-02-05 16:10:57 +0100

    test

--- app/core/Router.php
+++ app/core/Router.php
@@ -11,8 +11,8 @@ class Router{
     {
         $route = preg_replace('/\//', '\\/', $route);
         $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
-        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
-        $route = '/^' . $route . '$/i';
+        $route = preg_replace('/\{([a-z]+):([^}]+)\}/', '(?P<\1>\2)', $route);
+        $route = '#^' . $route . '$#i';
 
         $this->routes[$route] = $params;
     }
